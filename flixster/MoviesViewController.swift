b06fc110b3a845bbc45f3e5ab7c839365c61cc48
Walter C <walter.condori.10@gmail.com>
2020-09-25 23:18:41 -0700

    Adjusted posterView setImage

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -64,9 +64,9 @@ class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDe
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
-        let posterUrl = URL(string: baseUrl + posterPath)!
+        let posterUrl = URL(string: baseUrl + posterPath)
         
-        cell.posterView.af_setImage(withURL: posterUrl)
+        cell.posterView.af.setImage(withURL: posterUrl!)
         
         return cell
     }
